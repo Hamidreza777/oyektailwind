bbdc143d431408a40cfa881fe67f5bd1e39f2730
Artin <m.malekii2003@gmail.com>
2023-05-24 17:59:16 +0430

    artin

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,4 +1,18 @@
 import lamp from "../../assets/pic/Final photo.png";
+import dising from '../../assets/pic/Web design.png';
+import ui from '../../assets/pic/ux ui.png';
+import webapp from '../../assets/pic/Webapp.png';
+import mohtava from '../../assets/pic/mohtava.png';
+import ads from '../../assets/pic/ads.png';
+import seo from '../../assets/pic/seo.png';
+import about from '../../assets/pic/About.png';
+import pic1 from '../../assets/pic/Project summary.png';
+import pic2 from '../../assets/pic/final project.png';
+import pic3 from '../../assets/pic/Initial project.png';
+import team from '../../assets/pic/team members.png';
+
+
+
 
 export default function Body() {
   return (
@@ -22,20 +36,145 @@ export default function Body() {
       </div>
 
       <div className="flex justify-center">
-      <p className="text-[16px] text-avaliye font-bold mt-[-30%]" >خدمات اُیک</p>
+      <p className="text-[16px] text-avaliye font-bold mt-[-30%] desktop:mt-[-20%]" >خدمات اُیک</p>
       </div>
         
 
-      <div className="w-full grid grid-cols-2 place-items-center gap-2 ">
-        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox"></div>
-        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox"></div>
-        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox"></div>
-        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox"></div>
-        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox"></div>
-        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox"></div>
+      <div className="w-[85%] mx-auto grid grid-cols-2 place-items-center gap-2 mt-[-18%] desktop:flex desktop:justify-between desktop:mt-[-13%] ">
+        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox  bg-white grid place-items-center">
+<img className="h-[72px] w-[72px]" src={dising} alt="" />
+<p className="text-[14px] font-bold">طراحی سایت</p>
+
+        </div>
+        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox bg-white grid place-items-center desktop:mb-[-13%]">
+
+<img className="h-[72px] w-[72px]" src={ui} alt="" />
+<p className="text-[14px] font-bold ">رابطه و تجربه کاربری</p>
+        </div>
+        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox bg-white grid place-items-center desktop:mb-[-23%]">
+
+<img  className="h-[72px] w-[72px]" src={webapp} alt="" />
+<p className="text-[14px] font-bold">وب اپلیکشن</p>
+        </div>
+        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox bg-white grid place-items-center desktop:mb-[-23%]">
+
+<img  className="h-[72px] w-[72px]" src={mohtava} alt="" />
+<p className="text-[14px] font-bold">تولید محتوا</p>
+        </div>
+        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox bg-white grid place-items-center desktop:mb-[-13%]">
+
+<img  className="h-[72px] w-[72px]" src={ads} alt="" />
+<p className="text-[14px] font-bold">تبلیغات گوگل </p>
+        </div>
+        <div className="h-[128px] w-[128px] rounded-[5px] shadow-mybox bg-white grid place-items-center">
+
+<img  className="h-[72px] w-[72px]" src={seo} alt="" />
+<p className="text-[14px] font-bold">سئو</p>
+        </div>
 
 
       </div>
+
+
+<p className="text-[16px] text-avaliye font-bold mt-[15%] flex justify-center">درباره اُیک</p>
+
+
+
+
+
+
+
+<div className="w-full h-[443px] bg-avaliye flex justify-center mt-[16px] desktop:h-[600px] ">
+<div className="w-[85%] flex items-center justify-between flex-wrap ">
+
+<p className="font-bold text-[16px] text-white  desktop:mt-[-10%] desktopl:order-1">که هستیم؟</p>
+<img className="w-[193px h-[165px] desktop:w-[480px] desktop:h-[400px] desktop:mt-[10%] desktopl:order-3" src={about} alt="" />
+
+
+<p className="text-[14px] text-white font-normal desktop:w-[322px] desktop:mt-[-50%] desktopl:order-2 desktopl:mt-[15%] desktopl:mr-[-40%] leading-[21.88px]">
+
+امروزه بیش تر کسب و کار ها اینترنتی شده است اگر شما همچنان تردید دارید به راه اندازی کسب و کار اینترنتی گروه پشتیبانی اُ یک درخدمت شما است تا بهترین راهنمایی را به شما انجام دهد . گروه اُ یک ۰ تا ۱۰۰ سایت شمارو (پشتیبانی ، طراحی سایت ، سئو سایت ، ui ux سایت و ...) انجام می‌دهد و همچنین با داشتن نمونه کار های متعدد باعث رضایت بسیاری از مراجعه کنندگان بوده است.
+</p>
+</div>
+
+
+
+ </div>
+
+
+
+
+
+
+
+
+<p className="text-[28px] text-avaliye font-bold mt-[200px] flex justify-center">مراحل طراحی سایت</p>
+<div className="w-[75%] flex flex-wrap justify-center  mx-auto h-[1086px] mt-[16px] desktop:justify-between desktop:h-[352px] desktopl:w-[60%] ">
+
+<div  className="h-[352px] w-[237px] bg-gradient-to-t from-white to-cyan-200  mt-[10px] rounded-t-[150px]  flex items-center justify-center flex-col">
+
+<img className="h-[88px] w-[88px]   " src={pic1} alt="" />
+<p className="text-[14px] font-bold mt-[5%] ">
+دریافت خلاصه پروژه
+</p>
+<p className="w-[187px] text-[14px] font-normal mt-[5%] leading-[21.88px] ">
+این یک سند کوتاه یک یا دو صفحه ای است که اهداف سایت : چالش ها ، پیام رسانی ، راه حل و سایر جزئیات یک پروژه را مشخص می‌کند همچنین به راهنمایی تیم خلاق اُ یک درمورد چه گونگی دستیابی به اهداف پروژه کمک می‌کند.
+
+</p>
+
+</div>
+<div className="h-[352px] w-[237px] bg-gradient-to-t from-white to-orange-200 mt-[10px]  rounded-t-[150px]  flex items-center justify-center flex-col">
+<img className="h-[88px] w-[60.6px]   " src={pic2} alt="" />
+<p className="text-[14px] font-bold mt-[5%] ">
+ارائه نسخه اولیه
+</p>
+<p className="w-[187px] text-[14px] font-normal mt-[5%] leading-[21.88px]">
+در این مرحله ما نسخه اولیه از مطالب خواسته شده را در اختیار شما قرار می‌دهیم تا از روند انجام پروژه مطلع باشید.
+
+</p>
+
+</div>
+<div className="h-[352px] w-[237px] bg-gradient-to-t from-white to-green-200  mt-[10px]  rounded-t-[150px]  flex items-center justify-center flex-col ">
+<img className="h-[100px] w-[72px]   " src={pic3} alt="" />
+<p className="text-[14px] font-bold mt-[5%] ">
+ارائه نسخه نهایی
+
+</p>
+<p className="w-[187px] text-[14px] font-normal mt-[5%] leading-[21.88px] ">
+بعد از تکمیل پروژه یک فایل تست در اختیار شما قرار می‌گیرد تا به بررسی پروژه بپردازید و در صورت مشکل به ما اطلاع بدهید تا تیم اُ یک به بررسی مشکل ایجاد شده بپردازد.
+</p>
+
+
+
+</div>
+</div>
+
+<p className="text-avaliye font-bold text-[16px]  mt-[96px]  flex justify-center">اعضای تیم اُیک</p>
+
+{/* <div className="h-[420px] w-full flex flex-row-reverse">
+<div className="bg-sanaviye w-[70px] h-[119px] mt-[30%] ml-[24px]">
+
+</div>
+<img className="h-[148px] w-[246px] z-30 mt-[7%] ml-[-7%]" src={team} alt="" />
+
+
+<div className="bg-avaliye w-[242px] h-[267px] z-50 mt-[35%] ml-[-45%] mtablet:hidden">
+
+<p className="text-white font-bold text-[14px] mt-[16px]">
+همه اعضای تیم ما حرفه ایی هستند
+
+</p>
+
+<p className="text-white font-normal text-[14px]  mt-[16px]" >
+امروزه بیش تر کسب و کار ها اینترنتی شده است اگر شما همچنان تردید دارید به راه اندازی کسب و کار اینترنتی گروه پشتیبانی اُ یک درخدمت شما است تا بهترین راهنمایی را به شما انجام دهد . گروه اُ یک ۰ تا ۱۰۰ سایت شمارو (پشتیبانی ، طراحی سایت ، سئو سایت ، ui ux سایت و ...)
+
+</p>
+
+
+
+</div>
+
+</div>  */}
 
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
+      mtablet:"768px",
       tablet: "1200px",
       desktop: "1024px",
       desktopl:"1440px"
