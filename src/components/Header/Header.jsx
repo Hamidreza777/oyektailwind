e0c47e6d342741011f94229491f87b1304bf9570
hamidreza soltani <h.soltani8452@gmail.com>
2023-06-05 00:47:34 +0430

    dropdown1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,26 +4,92 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 import Logo from "../../assets/pic/logo2.png";
 import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState} from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
-
+import { IoIosArrowDown,IoIosArrowUp } from "react-icons/io";
 
 export default function Header() {
-const[Menu,setMenu]=useState("hidden")
 
 
 
+  const [Close,setClose]=useState("hidden");
+  const [Ham,setHam]=useState("max-desktop:flex");
+
+  const [Top,setTop]=useState("absolute top-[-130%]");
+
+  const [Dur,setDuration]=useState("duration-700");
+
+  const [rotate,setRotate]=useState("block");
+
+  const [rotateu,setRotateu]=useState("hidden");
+
+
+  
+  let Crotate;
+
+  const changrotate=()=>{
+
+if(Crotate){
+  setRotate("hidden") ;
+  setRotateu("block");
+  
+  console.log("hi")
+  Crotate=false;
+
+
+}
+
+
+else {
+
+  setRotate("block") ;
+  setRotateu("hidden");
+
+  console.log("bye");
+  
+  Crotate=true;
+
+}
+
+
+  }
+
+
+
+
+  const changeClose=()=>{
+      setClose("block");
+      setHam("max-desktop:hidden");
+      setTop("absolute top-[0px]");
+      setDuration("duration-700");
+  }
+
+
+  const changeClose2=()=>{
+    setClose("hidden");
+    setHam("max-desktop:flex");
+    setTop("absolute bottom-[150%]");
+    setDuration("duration-700");
 
 
   }
   return (
 
     <>
-      <header className="flex h-[4.875rem] w-full bg-sanaviye items-center justify-center fixed z-50">
+
+
+      <header className="flex h-[4.875rem] w-full bg-sanaviye items-center justify-center fixed z-50 ">
+
+      
         <nav className="flex flex-row items-center justify-between w-[85%] h-full tablet:w-[85%] transition ease-in-out delay-100 max-desktop:justify-between">
       
-        <RxHamburgerMenu className="text-[26px] desktop:hidden max-desktop:flex max-desktop:order-1"/>
-         
+        <RxHamburgerMenu onClick={changeClose} className={`text-[26px] desktop:hidden  max-desktop:order-1 ${Ham}`}/>
+       
+        <AiOutlineClose onClick={changeClose2} className={`text-[26px] text-black desktop:hidden  ${Close}`}/>
+
+ 
+     
           <img className="h-[36px] max-desktop:order-3 max-desktop:ml-[10%]" src={Logo} alt="" />
 
           <ul className="flex flex-row justify-between w-[40%] h-full items-center max-desktop:hidden">
@@ -70,14 +136,31 @@ const[Menu,setMenu]=useState("hidden")
 
       </header>
 
-<div className="w-full h-[324px] z-50 bg-white   hidden max-desktop:block   "  >
 
-<ul className="pt-[25%]  h-full items-center mr-[5%] ">
+
+
+
+
+
+
+      <div className={`mtablet:h-[45vh] mobile:h-[47vh] bg-white w-full h-[48%] z-40 hidden max-desktop:block rounded-[5px]  ${Dur} ${Top}`} >
+
+
+
+
+
+
+
+<ul className="mtablet:pt-[12%] mobile:pt-[18%] pt-[25%] w-full h-full items-center  pr-[7%]  ">
             <li className=" w-[30%]  hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
               <Link to="/">صفحه اصلی</Link>
             </li>
-            <li className="w-[20%] mt-[16px] hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
-              <Link to="/Khadamat">خدمات ما</Link>
+            <li onClick={changrotate} className="flex items-center w-[25%] mt-[16px] hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
+              <Link >خدمات ما</Link>
+              
+              <IoIosArrowDown className={`mr-[10%] ${rotate}`} />
+             < IoIosArrowUp className={`mr-[10%] ${rotateu}`}/>
+
             </li>
             <li className="w-[20%] mt-[16px]   hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
               <Link to="/Amoozesh">آموزش</Link>
@@ -95,10 +178,10 @@ const[Menu,setMenu]=useState("hidden")
 
 
 
-
       <Outlet />
 
   
+
 
 
     </>
