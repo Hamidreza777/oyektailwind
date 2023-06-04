761092c94ef3d3621401549598984c63cb5435aa
hamidreza soltani <h.soltani8452@gmail.com>
2023-06-04 13:08:36 +0430

    menu2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,24 +4,51 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 import Logo from "../../assets/pic/logo2.png";
 import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
-
+import { useState} from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 
 export default function Header() {
-const [Menu,setMenu]=useState("hidden");
 
 
 
+  const [Close,setClose]=useState("hidden");
+  const [Ham,setHam]=useState("max-desktop:flex");
+
+  const [Top,setTop]=useState("absolute top-[-130%]");
+
+  const [Dur,setDuration]=useState("duration-700");
 
 
+
+  const changeClose=()=>{
+      setClose("block");
+      setHam("max-desktop:hidden");
+      setTop("absolute top-[0px]");
+      setDuration("duration-700");
+  }
+
+
+  const changeClose2=()=>{
+    setClose("hidden");
+    setHam("max-desktop:flex");
+    setTop("absolute bottom-[150%]");
+    setDuration("duration-700");
+
+
+  }
   return (
+
     <>
-      <header className="flex h-[4.875rem] w-full bg-sanaviye/30 items-center justify-center fixed z-50 backdrop-blur-md">
+
+
+      <header className="flex h-[4.875rem] w-full bg-sanaviye items-center justify-center fixed z-50 ">
+
+      
         <nav className="flex flex-row items-center justify-between w-[85%] h-full tablet:w-[85%] transition ease-in-out delay-100 max-desktop:justify-between">
       
-        <RxHamburgerMenu className="text-[26px] desktop:hidden max-desktop:flex max-desktop:order-1"/>
-    
+        <RxHamburgerMenu onClick={changeClose} className={`text-[26px] desktop:hidden  max-desktop:order-1 ${Ham}`}/>
+        
  
      
           <img className="h-[36px] max-desktop:order-3 max-desktop:ml-[10%]" src={Logo} alt="" />
@@ -70,13 +97,27 @@ const [Menu,setMenu]=useState("hidden");
 
       </header>
 
-<div className="w-full h-[324px] z-50 bg-white   hidden max-desktop:block   "  >
 
-<ul className="pt-[25%]  h-full items-center mr-[5%] ">
+
+
+
+
+
+
+      <div className={`mtablet:h-[35vh] mobile:h-[35vh] fixed bg-white w-full h-[48%] z-50 hidden max-desktop:block absolute  ${Dur} ${Top}`} >
+
+      <AiOutlineClose onClick={changeClose2} className={`text-[26px] text-black desktop:hidden absolute top-[9%] right-[6%] ${Close}`}/>
+
+
+
+
+
+
+<ul className="mtablet:pt-[10%] mobile:pt-[10%] pt-[20%] w-full h-full items-center  pr-[7%] ">
             <li className=" w-[30%]  hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
               <Link to="/">صفحه اصلی</Link>
             </li>
-            <li className="w-[20%] mt-[16px]   hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
+            <li className="w-[20%] mt-[16px] hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
               <Link to="/Khadamat">خدمات ما</Link>
             </li>
             <li className="w-[20%] mt-[16px]   hover:text-avaliye hover:border-b-[2px] hover:border-avaliye hover:transition ease-in-out delay-100">
@@ -92,6 +133,9 @@ const [Menu,setMenu]=useState("hidden");
 
 
 </div>
+
+
+
 
       <Outlet />
 
